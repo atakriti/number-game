e2a73c545b3288867e7fb19cb83e1d41f39e9f64
atakriti <anwart256@gmail.com>
2022-06-11 13:08:35 +0200

    result points done

--- src/js/index.js
+++ src/js/index.js
@@ -1,27 +1,39 @@
 let container = document.querySelector(".container");
 let container2 = document.querySelector(".container2");
+let container3 = document.querySelector(".container3");
 let span = document.querySelector(".number");
 let span2 = document.querySelector(".number2");
+let span3 = document.querySelector(".number3");
 let empty = "";
 let empty2 = "";
+let empty3 = "";
 let strong = document.querySelector(".timer1");
 let strong2 = document.querySelector(".timer2");
+let strong3 = document.querySelector(".timer3");
 let movedElement = document.querySelector(".movedElement")
-let movedElement2 = document.querySelector(".movedElement2")
+let userNameResult = document.querySelector(".userNameResult")
+let resultSpan = document.querySelector(".resultSpan")
+
+// let movedElement2 = document.querySelector(".movedElement2")
 // ===================
 let level1 = document.querySelector(".level1")
 level1.style.display = "none"
 let level2 = document.querySelector(".level2")
-// level2.style.display = "none"
+level2.style.display = "none"
+let level3 = document.querySelector(".level3")
+level3.style.display  ="none"
 let welcomePage = document.querySelector(".welcome-page")
-welcomePage.style.display = "none"
+// welcomePage.style.display = "none"
 let loading = document.querySelector(".loading")
 loading.style.display = "none"
+let result = document.querySelector(".result")
+result.style.display = "none"
 
-
-
+let yesBtn = document.getElementById("yesBtn")
+let noBtn = document.getElementById("noBtn")
 let nextBtn = document.querySelector("#next")
 let nextBtn2 = document.querySelector("#next2")
+let nextBtn3 = document.querySelector("#next3")
 let timeOut = document.querySelector(".timeOut")
 timeOut.style.display = "none"
 // ======================= level one Function ======================
@@ -63,32 +75,32 @@ function level1Fun() {
 level1Fun();
 // =================================== end level1 =====================
 // ============================ timecounter ================
-let timeVariable = 5
+// let timeVariable = 2
 
-function timerFunction() {
+// function timerFunction() {
   
-    strong.innerHTML = timeVariable--
+//     strong.innerHTML = timeVariable--
    
   
-  if (strong.innerHTML == 0) {
-    strong.remove()
-     timeOut.style.display = "block"
-     setTimeout(() => (timeOut.style.display = "none"),5000)
-       // timerVar = 5
-   }
+//   if (strong.innerHTML == 0) {
+//     strong.remove()
+//      timeOut.style.display = "block"
+//      setTimeout(() => (timeOut.style.display = "none"),5000)
+//        // timerVar = 5
+//    }
  
-}
-function timerCall() {
-  if (level1.style.display === "block") {
-    timerFunction()
-  }
-}
+// }
+// function timerCall() {
+//   if (level1.style.display === "block") {
+//     timerFunction()
+//   }
+// }
 
-setInterval(timerCall, 1000)
+// setInterval(timerCall, 1000)
 // ==================== end timecounter ===================
 // ==================== level2 =================================
 function level2Fun() {
-  for (let i = 0; i < 50; i++){
+  for (let i = 0; i < 40; i++){
     let div = document.createElement("div")
     let random = Math.ceil(Math.random() * 10)
     let divContent = document.createTextNode(random)
@@ -103,7 +115,7 @@ function level2Fun() {
       }
       if (empty2 === e.target.innerText) {
         movedElement.appendChild(div)
-        console.log(movedElement)
+
       } else {
         span2.innerHTML = "Wrong!, try again";
         span2.style.color = "red";
@@ -111,21 +123,107 @@ function level2Fun() {
     })
   }
 
-  // nextBtn2.addEventListener("click", () => {
-  //   document.querySelector(".level2").style.display = "none"
-  //   document.querySelector(".loading").style.display = "block"
-  //   setTimeout(() => (document.querySelector(".loading").style.display = "none"), 5000)
-  // // here comes the level 2    
-  // })
+  nextBtn2.addEventListener("click", () => {
+    document.querySelector(".level2").style.display = "none"
+    document.querySelector(".loading").style.display = "block"
+    setTimeout(() => (document.querySelector(".loading").style.display = "none"), 5000)
+    setTimeout(() => (document.querySelector(".level3").style.display = "block"), 5000)
+
+  })
 }
 level2Fun();
 // ==================== end Level2 ==========================
+// =============== level2 timer ==============
+// let timeVariable2 = 2
+
+// function timerFunction2() {
+  
+//     strong2.innerHTML = timeVariable2--
+   
+  
+//   if (strong2.innerHTML == 0) {
+//     strong2.remove()
+//      timeOut.style.display = "block"
+//      setTimeout(() => (timeOut.style.display = "none"),5000)
+//        // timerVar = 5
+//    }
+ 
+// }
+// function timerCall2() {
+//   if (level2.style.display === "block") {
+//     timerFunction2()
+//   }
+// }
+
+// setInterval(timerCall2, 1000)
+// ============================================
+// ============================== level 3 =====================
+function level3Fun() {
+  for (let i = 0; i < 70; i++){
+    let div = document.createElement("div")
+    let random = Math.ceil(Math.random() * 10)
+    let divContent = document.createTextNode(random)
+    div.appendChild(divContent)
+    container3.appendChild(div)
+    div.addEventListener("click", (e) => {
+      if (empty3 == "") {
+        empty3 += e.target.innerText;
+        span3.innerHTML = empty3;
+        span3.style.color = "red"
+      }
+      if (empty3 === e.target.innerText) {
+        movedElement.appendChild(div)
+        console.log(movedElement)
+        resultSpan.innerHTML = movedElement.innerHTML.length
+        console.log(resultSpan)
+
+      } else {
+        span3.innerHTML = "Wrong!, try again";
+        span2.style.color = "red";
+      }
+    })
+  }
+  nextBtn3.addEventListener("click", () => {
+    document.querySelector(".level3").style.display = "none"
+    document.querySelector(".loading").style.display = "block"
+    setTimeout(() => (document.querySelector(".loading").style.display = "none"), 5000)
+    setTimeout(() => (result.style.display = "block"), 5000)
+
+  })
+}
+level3Fun()
+// =========================== end level3 ==================
+// =============== level2 timer ==============
+// let timeVariable3 = 2
+
+// function timerFunction3() {
+  
+//     strong3.innerHTML = timeVariable3--
+   
+  
+//   if (strong3.innerHTML == 0) {
+//     strong3.remove()
+//      timeOut.style.display = "block"
+//      setTimeout(() => (timeOut.style.display = "none"),5000)
+//        // timerVar = 5
+//    }
+ 
+// }
+// function timerCall3() {
+//   if (level3.style.display === "block") {
+//     timerFunction3()
+//   }
+// }
+
+// setInterval(timerCall3, 1000)
+// ============================================
 // ===================== welcome function ======================
 function welcome(e) {
   e.preventDefault()
   let inputValue = document.getElementById("welcome-input").value
   if (!inputValue == "") {
-      document.querySelector(".userName").innerHTML = inputValue[0] + inputValue.slice(1)
+      document.querySelector(".userName").innerHTML = inputValue
+      userNameResult.innerHTML = inputValue
       welcomePage.style.display = "none"
       loading.style.display = "block"
       setTimeout(() => (loading.style.display = "none"), 5000)
@@ -133,4 +231,11 @@ function welcome(e) {
       
   }
 }
-document.getElementById("formWelcome").addEventListener("submit",welcome)
+document.getElementById("formWelcome").addEventListener("submit", welcome)
+
+// ========================= Result ======================
+// function resultFunction() {
+//   resultSpan.innerHTML += movedElement.innerHTML.length
+//   console.log(movedElement)
+// }
+// resultFunction()
